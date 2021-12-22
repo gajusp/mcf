4121fcbde603332bb6e5d2abc886fd9965abc403
Gajanan <gaju.sp@gmail.com>
2021-12-22 21:09:52 +0000

    Create react app
    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className='app-header'>My Community Finance - Address component test</header>
+
+      <div className='main-container'></div>
+    </div>
+  );
+}
+
+export default App;
